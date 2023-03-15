e63fec33c25deed63660e40159ef49c051c6dfa0
Sarvinoz <sarvinozkaumova8@gmail.com>
2023-03-15 20:23:11 +0500

    first commit

---
+++ right on target/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  right on target
+//
+//  Created by Sarvinoz Kayumova on 15/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
